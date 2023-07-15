7c3e6bdd62ff6fdd4718ecb5e2191499c00f86d7
Haydee <haydeeccb@gmail.com>
2023-07-14 20:33:43 -0400

    Cambios estéticos

--- PruebaOrtografia.kt
+++ PruebaOrtografia.kt
@@ -6,27 +6,32 @@ fun main() {
 	var intentos = 0
 	var ayudanteOrtografico = AyudanteOrtografico()
 	println("Bienvenido al programa de ayudante ortográfico")
+	println(" ")
 	while (salirAplicacion == false && intentos < 3) {
-		println("Por favor seleccione una opción a ejecutar")
+		println("Por favor seleccione una opción a ejecutar:")
 		println("1. Crear un nuevo ayudante ortográfico")
 		println("2. Cargar un diccionario")
 		println("3. Eliminar palabra")
 		println("4. Corregir texto")
 		println("5. Mostrar diccionario")
 		println("6. Salir de la aplicación")
+		print("Opción: ")
 		var accion = readLine()
 		when(accion) {
 			"1" -> {
 				ayudanteOrtografico = AyudanteOrtografico()
 				println("Fue creado un nuevo ayudante ortográfico")
+				println(" ")
 			}			
 			"2" -> {
+				print("Por favor ingrese el nombre del archivo a cargar: ")
 				var fname = readLine()
 				if (fname == null) {
 					println("El nombre ingresado no es válido. El nombre del archivo no puede ser vacío")
 				} else {
 					ayudanteOrtografico.cargarDiccionario(fname)
 				}
+				println(" ")
 			}
 			"3" -> {
 				println("Por favor ingrese la palabra a eliminar")
@@ -36,6 +41,7 @@ fun main() {
 				} else {
 					ayudanteOrtografico.borrarPalabra(palabraEliminar)
 				}
+				println(" ")
 			}
 			"4" -> {
 				println("Por favor ingrese el nombre del archivo a corregir")
@@ -51,13 +57,18 @@ fun main() {
 						ayudanteOrtografico.corregirTexto(finput, foutput)
 					}
 				}
+				println(" ")
 			}
 			"5" -> ayudanteOrtografico.imprimirDiccionario()
-			"6" -> salirAplicacion = true
+			"6" -> {
+				salirAplicacion = true
+				println(" ")
+			}
 			else -> {
 				intentos++
 				println("La opción ingresada no es válida")
 				println("Quedan ${3-intentos} oportunidades para ingresar una opción válida o se saldrá del programa")
+				println(" ")
 			}
 		}
 	}
